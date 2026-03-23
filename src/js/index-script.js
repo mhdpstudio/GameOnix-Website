3cd53130836f1622f5a28ca891b1e7b15cf2233a
MhdP Studio <mhdp.studio@gmail.com>
2026-03-23 16:15:16 +0200

    save

--- src/js/index-script.js
+++ src/js/index-script.js
@@ -111,9 +111,9 @@ function renderSections(filterType = 'all') {
                         <div class="game-card" data-slug="${game.slug}">
                             <div class="game-details">
                                 <img 
-                                    src="${game.poster ? game.poster + ".webp" : 'assets/images/game.jpg'}"
+                                    src="${game.poster ? game.poster + ".webp" : '../assets/images/game.jpg'}"
                                     alt="${game.title}"
-                                    onerror="this.src='assets/images/game.jpg'"
+                                    onerror="this.src='../assets/images/game.jpg'"
                                 >
                                 <div class="publisher">${game.publisher || WebsiteName}</div>
                                 <div class="title">${game.title}</div>
